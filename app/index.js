15f93df63ec0386eae989d878d253dddbe8091f3
Andy Barron <AndrewLBarron@gmail.com>
2018-01-08 23:51:42 -0800

    reply with image as well

--- app/index.js
+++ app/index.js
@@ -1,26 +1,57 @@
+/* eslint require-await: 0 */
 'use strict';
 const axios = require('axios');
 const Koa = require('koa');
 const Router = require('koa-router');
 const queryString = require('querystring');
+const util = require('util');
+const xml2js = require('xml2js');
 const { PORT, SLACK_CLIENT_ID, SLACK_CLIENT_SECRET } = require('./config');
 
 const API_URL = 'https://catfact.ninja/fact';
+const IMAGE_XML_URL = 'https://thecatapi.com/api/images/get?format=xml';
 const SLACK_OAUTH_URL = 'https://slack.com/api/oauth.access';
 
 const app = new Koa();
 const router = new Router();
 
-const getFactJson = async () => {
-  return (await axios(API_URL)).data;
+const getFactText = async () => {
+  return (await axios(API_URL)).data.fact;
+};
+
+const objectFromXmlString = util.promisify(xml2js.parseString);
+
+const getImageData = async () => {
+  const result = await axios({
+    responseType: 'document',
+    url: IMAGE_XML_URL,
+  });
+  const xml = result.data;
+  const parsed = await objectFromXmlString(xml);
+  const image = parsed.response.data[0].images[0].image[0];
+  return {
+    sourceUrl: image.source_url[0],
+    url: image.url[0],
+  };
 };
 
 const slackHandler = async (ctx) => {
-  const { fact } = await getFactJson();
+  const [fact, imageData] = await Promise.all([getFactText(), getImageData()]);
+  /* eslint-disable camelcase */
+  const attachments = [
+    {
+      author_link: imageData.sourceUrl,
+      author_name: 'Meow!',
+      fallback: 'Meow!',
+      image_url: imageData.url,
+    },
+  ];
   ctx.body = {
-    response_type: 'in_channel', // eslint-disable-line camelcase
+    attachments,
+    response_type: 'in_channel',
     text: fact,
   };
+  /* eslint-enable camelcase */
 };
 
 router.get('/', async (ctx) => {
@@ -31,6 +62,7 @@ router.get('/health', async (ctx) => {
   ctx.body = 'healthy';
 });
 
+router.get('/slack', slackHandler);
 router.post('/slack', slackHandler);
 router.get('/slack/authorize', async (ctx) => {
   const { code } = ctx.query;
